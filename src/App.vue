5d1366b2363877ddd568fecf1794a7c843fca4ea
BekzodSharipbaev <python.bekzod@gmail.com>
2024-01-29 08:14:42 +0500

    Первоначальный коммит

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-white w-4/5 m-auto mt-14 h-screen rounded-xl shadow-xl">
+    <header>
+      <div>
+        <img src="/logo.png" alt="Logo" />
+        <div>
+          <h2>Vue Sneakers</h2>
+          <p>Магазин лучших кроссовок</p>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script setup></script>
+
